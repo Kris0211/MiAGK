4b009cb1b9508d18ce55267bc38467d7abe25efb
Kris <stawinogakris@gmail.com>
2025-03-19 21:53:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 constexpr int imgWidth = 512;
 constexpr int imgHeight = 512;
 
-constexpr int A = 0xff;
-constexpr int R = 0x80;
-constexpr int G = 0x00;
-constexpr int B = 0xff;
+constexpr unsigned int A = 0xff;
+constexpr unsigned int R = 0x80;
+constexpr unsigned int G = 0x00;
+constexpr unsigned int B = 0xff;
 
-constexpr int COLOR = A << 24 | R << 16 | G << 8 | B;
+constexpr unsigned int COLOR = A << 24 | R << 16 | G << 8 | B;
 
 int main(int argc, char* argv[])
 {
